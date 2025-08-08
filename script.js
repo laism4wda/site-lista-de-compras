91086d94aedfc03f073398dee6bd073b2841d98c
laism4wda <lais.sayuri.maeda@gmail.com>
2025-08-08 14:46:19 -0300

    Add files via upload
    Signed-off-by: laism4wda <lais.sayuri.maeda@gmail.com>

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 import {CriarItemDaLista} from "./Scripts/CriarItemDaLista.js";
 //salva o elemento input dentro da variável inputItem
 const inputItem = document.getElementById("input-item");
-const listaDeCompras = document.getElementById("listaDeCompras");
+const listaDeCompras = document.getElementById("ListaDeCompras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 //Adiciona um evento fofoqueiro que ficará "escutando" quando um evento de clique no "botãoAdicionar" dor acionado
-botaoAdicionar.addEventListener("click", (evento) => {
+BotaoAdicionar.addEventListener("click", (evento) => {
     //Evita que a página atualize todos que todos os dado sejam perdidos
     evento.preventDefault();
     //Chama uma função que vai criar o item da lista
-    const itemDaLista = CriarItemDaLista();
+    const ItemDaLista = CriarItemDaLista();
     //Adiciona esse item criado dentro de da lista de compras
-    listaDeCompras.appendChild(itemDaLista);
+    ListaDeCompras.appendChild(TtemDaLista);
     //Chama a função que vai verificar se a lista está ou não vazia
-    verificarListaVazia(listaDeCompras);
+    verificarListaVazia(ListaDeCompras);
     inputItem.value = ""; 
 }); //Limpa o campo input ao clicar no botão
